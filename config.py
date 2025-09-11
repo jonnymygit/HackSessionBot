8cccf29e3c90c7dc88f63785a0e8e1ebc5db0e88
Boom boom bro <219003520+Probotsvip@users.noreply.github.com>
2025-09-11 15:58:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    API_ID = "13146123"
-    API_HASH = "53e10a257baaddea9c413a0c4249e731"
-    TOKEN = "6355813392:AAHojXdDT9pmxQV9xI1Px9i5i9KtVs-Fh_Y"  
+    API_ID = "22947426"
+    API_HASH = "3ac67de232f419724b3c905e1934bc7b"
+    TOKEN = "8498641388:AAECGA9Vg37D9G2LjDyRXBURViyG6zvkX6g"  
     START_PIC = "https://files.catbox.moe/24weaj.jpg"
