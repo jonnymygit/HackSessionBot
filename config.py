ca1462d68e451b589f3c715565eb31e218c8596c
Boom boom bro <219003520+Probotsvip@users.noreply.github.com>
2025-09-11 15:13:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    API_ID = "13146123"
+    API_HASH = "53e10a257baaddea9c413a0c4249e731"
+    TOKEN = "6355813392:AAHojXdDT9pmxQV9xI1Px9i5i9KtVs-Fh_Y"  
+    START_PIC = "https://telegra.ph/file/7e3be28a9b1bbd2b53e48.jpg"
