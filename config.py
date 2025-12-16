1e70c9d96fd36f7de3e51487bde8869819298cd6
jonnymygit <jonnyjames9022@gmail.com>
2025-12-16 22:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     API_ID = "22947426"
     API_HASH = "3ac67de232f419724b3c905e1934bc7b"
-    TOKEN = "8498641388:AAECGA9Vg37D9G2LjDyRXBURViyG6zvkX6g"  
+    TOKEN = "8565564772:AAERY3FFljUzrzGlAb389AGyXv-85xHUWEw"  
     START_PIC = "https://files.catbox.moe/24weaj.jpg"
